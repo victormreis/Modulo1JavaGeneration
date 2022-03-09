12058805b3fefe8edc23ad6ee7d55ed7ffee1cfe
Victor Martins <vitor_jhoul@hotmail.com>
2022-03-09 11:10:36 -0300

    Adicionando polimorfismo ao exercicio

---
+++ Heranca/Ex02.java
@@ -0,0 +1,20 @@
+package Heranca;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		Cachorro dog = new Cachorro();
+		Cavalo c1 = new Cavalo();
+		Preguica p1 = new Preguica();
+		
+		
+		dog.emitirSom();
+		dog.locomover();
+		c1.emitirSom();
+		c1.locomover();
+		p1.emitirSom();
+		p1.locomover();
+
+	}
+
+}
